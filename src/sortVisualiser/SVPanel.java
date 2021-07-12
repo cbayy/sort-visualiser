47429b505e08d4f5e6e8aa1f34d3d7321d7e744f
Cbayy <53809379+Cbayy@users.noreply.github.com>
2021-07-12 11:37:27 +0930

    Shuffles array before sorting.

--- src/sortVisualiser/SVPanel.java
+++ src/sortVisualiser/SVPanel.java
@@ -3,18 +3,30 @@ package sortVisualiser;
 import javax.swing.*;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.Random;
 
 public class SVPanel extends JPanel {
 
-    static int COL_X = 4;
+    static int COL_X = 10;
     static int BARS = visualiserDriver.WINDOW_X / COL_X;
-    public int[] arr = new int[100];
+    public int[] arr;
 
     public SVPanel(){
         //Sorted placeholder array
-        arr = new int[100];
+        arr = new int[BARS];
         for(int i = 0; i< arr.length; i++){
             arr[i] = i;
+        }
+        arrayRandom();
+    }
+
+    public void arrayRandom(){
+        Random r = new Random();
+        for(int i = 0; i < BARS; i++){
+            int index = r.nextInt(BARS-1);
+            int temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] =  temp;
         }
     }
 
@@ -23,7 +35,7 @@ public class SVPanel extends JPanel {
         Graphics2D g2D = (Graphics2D) g;
         super.paintComponent(g2D);
         for(int i = 0; i < arr.length; i++){
-            g2D.fillRect(i + COL_X * i,visualiserDriver.WINDOW_Y-arr[i],COL_X,arr[i]);
+            g2D.fillRect(i + COL_X * i,visualiserDriver.WINDOW_Y-arr[i]-100,COL_X,arr[i]);
         }
 
     }
