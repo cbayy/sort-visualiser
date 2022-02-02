17a1387b952a1dd8bd575ab1b96e1dd6ac475bc7
Cbayy <53809379+Cbayy@users.noreply.github.com>
2022-02-02 22:01:58 +1100

    Commented/cleaned code.

--- src/sortVisualiser/SortThread.java
+++ src/sortVisualiser/SortThread.java
@@ -1,9 +1,12 @@
 package sortVisualiser;
 
+//Sort thread runs whenever a new sort is started, to prevent the GUI from freezing while the algorithm runs
 public class SortThread implements Runnable{
 
     private VisualiserDriver.SortType currentSort;
     private SVPanel svPanel;
+
+    //Will return the time taken after thread ends
     public long elapsedTime;
 
     public SortThread(VisualiserDriver.SortType currentSort, SVPanel svPanel) {
@@ -11,11 +14,15 @@ public class SortThread implements Runnable{
         this.svPanel = svPanel;
     }
 
-
     public void run(){
+        //Randomises the array before a new sort is ran
         svPanel.arrayRandom();
+
+        //Prepares GUI for sort
         svPanel.setElapsedTime("N/A");
         VisualiserDriver.activateButtons(false);
+
+        //Runs a specific sort depending on currentSort(button pressed)
         long startTime = System.nanoTime();
         switch (currentSort) {
             case QS:
@@ -31,6 +38,7 @@ public class SortThread implements Runnable{
                 iSort.sort(svPanel);
                 break;
         }
+        //Calculates and returns time
         elapsedTime = System.nanoTime() - startTime;
         svPanel.setElapsedTime(String.valueOf(elapsedTime/1000000000));
         VisualiserDriver.activateButtons(true);
