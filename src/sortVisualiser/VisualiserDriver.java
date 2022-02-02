df305c37f9bca7d597a1e05387ade9d054f42ce5
Cbayy <53809379+Cbayy@users.noreply.github.com>
2022-02-02 21:39:54 +1100

    Commented/cleaned code.

--- src/sortVisualiser/VisualiserDriver.java
+++ src/sortVisualiser/VisualiserDriver.java
@@ -1,38 +1,41 @@
 package sortVisualiser;
 
 import javax.swing.*;
-import javax.sound.midi.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class VisualiserDriver extends Thread {
 
+    static VisualiserDriver vd;
     private final JFrame frame;
     private final SVPanel svPanel;
     private final JPanel conPanel;
 
+    //The control buttons(left of screen) start each sorting algorithm
     static JButton btnMergeSort;
     static JButton btnQuickSort;
     static JButton btnInsertionSort;
 
+    //Text which appears about the current sort
     private JLabel runTimeLabel;
     private SortType currentSort;
-    static VisualiserDriver vd;
 
     public static int WINDOW_X = 1200;
     public static int WINDOW_Y = 800;
 
+    //Current three types of sorts
     public enum SortType{MS, QS, IS}
 
     public static void main(String[] args){
         vd = new VisualiserDriver();
     }
 
+    //Sets up the frame
     public VisualiserDriver(){
+        //Sets basic frame information
         frame = new JFrame("Sort Visualiser");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1500,800);
-
         frame.setLayout(new BorderLayout());
 
         svPanel = new SVPanel();
@@ -42,10 +45,9 @@ public class VisualiserDriver extends Thread {
         frame.add(svPanel, BorderLayout.CENTER);
         frame.add(conPanel, BorderLayout.EAST);
         frame.setVisible(true);
-
     }
 
-
+    //Sets up the control panel located on the right
     public void setUpConPanel(){
         btnMergeSort = new JButton("Merge Sort");
         btnQuickSort = new JButton("Quick Sort");
@@ -64,6 +66,7 @@ public class VisualiserDriver extends Thread {
         conPanel.add(btnInsertionSort);
     }
 
+    // Handles request when any of the control buttons are pressed
     public void actionPerformed(ActionEvent e) {
 
         if(e.getSource() == btnMergeSort) {
@@ -79,13 +82,12 @@ public class VisualiserDriver extends Thread {
         SortThread t = new SortThread(currentSort, svPanel);
         System.out.println("Start");
         new Thread(t).start();
-
     }
 
+    //Deactivates buttons during sort/ reactivate afterwards
     public static void activateButtons(Boolean active){
         btnMergeSort.setEnabled(active);
         btnQuickSort.setEnabled(active);
         btnInsertionSort.setEnabled(active);
     }
-
 }
